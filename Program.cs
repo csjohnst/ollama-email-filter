0e84b2be4c33d617877c6153b81f9eeb2e64e70e
Chris Johnstone <chris@johnstone.id.au>
2024-06-03 15:25:48 +0800

    Remove unused classes

--- Program.cs
+++ Program.cs
@@ -173,20 +173,7 @@ class OllamaEmailFilter
 
 
     }
-    public class EmailSettings
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Host { get; set; }
-        public int Port { get; set; }
-    }
-
-    public class AppSettings
-    {
-        public string Uri { get; set; }
-        public string ModelName { get; set; }
-        public string PromptRatings { get; set; }
-    }
+    
 
     private class MyEmail
     {
